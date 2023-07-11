a5687251e26f1a93f5624ef17eb64f9b46a55949
Yogi120 <me.yogesh.pal@gmail.com>
2023-07-11 12:52:46 +0530

    Finding Prime Number or Not

---
+++ Palindrome_Number/Palindrome_Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number");
+            string Num = Convert.ToString(Console.ReadLine());
+
+            for(int i =0; i<Num.Length; i++)
+            {
+                if (Num[i] == Num[Num.Length - 1])
+                {
+                    Console.WriteLine("It's a Palindrome Number!");
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("It's NOT a Palindrome Number!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ Prime_number/Prime_number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i < Num; i++)
+            {
+                if (Num % i == 0)
+                {
+                    Console.WriteLine("It's Not a Prime Number");
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("It's a prime number");
+                    break;
+                }
+            }
+        }
+    }
+}
