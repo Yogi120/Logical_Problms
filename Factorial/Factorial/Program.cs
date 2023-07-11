77d43c7440ca9442779c50461816d836168b52cd
Yogi120 <me.yogesh.pal@gmail.com>
2023-07-11 12:55:44 +0530

    Factorial of given Number

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number: ");
+            int factorial = 1;
+            int Num = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i < Num; i++)
+            //{
+            //    factorial = factorial * i;
+
+            //}
+
+            int i = 1;
+
+            while (i < Num)
+            {
+                factorial = factorial * i;
+                i++;
+            }
+            Console.WriteLine(factorial);
+
+
+           
+        }
+    }
+}
---
+++ Swap_Number/Swap_Number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter num1: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Num2: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("New num1: " + a);
+            Console.WriteLine("New num2: " + b);   
+        }
+    }
+}
