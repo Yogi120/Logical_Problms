bdb8b311751bf3b8979bfd5219c135d3fff122df
Yogi120 <me.yogesh.pal@gmail.com>
2023-07-11 12:57:36 +0530

    Given number is Palindrom or Not

---
+++ Reverse_number/Reverse_number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            string ReverseNum = "";
+
+            while (num > 0)
+            {
+                int digit = num % 10;
+                //Console.WriteLine(digit);
+                ReverseNum = ReverseNum + digit;
+                num = num / 10;
+            }
+            Console.WriteLine(ReverseNum);
+        }
+    }
+}
