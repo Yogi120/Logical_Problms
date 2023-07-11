01678618607233055143f143e5ee7cc2ea66c859
Yogi120 <me.yogesh.pal@gmail.com>
2023-07-11 12:59:16 +0530

    Sum of given digit

---
+++ SumOfDigit/SumOfDigit/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            while (num > 0)
+            {
+                int remain = num % 10;
+                sum += remain;
+                num = num / 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
